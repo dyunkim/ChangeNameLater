352ab6f2efa681dcbba56cdbce34d505723e0fdb
tjk9b <tjk9b@mail.missouri.edu>
2018-12-07 01:02:28 -0600

    hello_flask

--- hello_flask.py
+++ hello_flask.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route('/comment', methods=['POST'])
+def add_comment():
+    comment = request.json['comment']
+    candidate = request.json['candidate']
+    return comment
+
+@app.route('/profile/<int:id>')
+def get_profile(id):
+    return "Profile: " + str(id)
+
+@app.route('/search', methods=['GET'])
+def search_profile():
+    first = request.args.get('first')
+    last = request.args.get('last')
+    return first + last
