a0b0131b3ea92cb2a240261cce754e75f3034eb2
dyunkim <davidyunkim@gmail.com>
2018-12-07 00:53:37 -0600

    added image post

--- hello_flask.py
+++ hello_flask.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+import os
+from PIL import Image
 app = Flask(__name__)
+
+UPLOAD_FOLDER = './uploads'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World!1"
+
+@app.route("/profile/<int:id>")
+def getProfile(id):
+    return "Profile: " + str(id)
+
+@app.route("/processPicture", methods=['POST'])
+def send_to_ocr():
+    image = request.files['image']
+    pil_image = Image.open(image)
+    f = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+    image.save(f)
+    print(pil_image)
+    return "Image Uploaded"
